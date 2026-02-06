b43d9a6a34ee422a5ed0968858ba78425103dc6e
sumit0704-bit <kumarsumit70406@gmail.com>
2026-02-06 23:30:34 +0530

    created ownerroute

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('config');
 const dbgr = require('debug')("development:mongoose");
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
     dbgr("connected");
 })
--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model")
+
+if(process.env.NODE_ENV==="development"){
+router.post("/create",async function(req,res){
+    let owners = await ownerModel.find();
+    if(owners.length>0){
+       return res.status(501).send("not allowed")
+    }
+    let{fullname,email,password}=req.body;
+    let createdowner = await ownerModel.create({
+        fullname,
+        email,
+        password
+    })
+    res.status(201).send(createdowner)
+})
+}
 
 router.get("/", function (req, res) {
     res.send("hey it's owner");
 });
+
+
 
 module.exports = router;
